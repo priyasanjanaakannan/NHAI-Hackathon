d8d86505370d440214984b839877a2bdd526f24a
priyasanjanaakannan <priyasanjanaakannan@gmail.com>
2026-06-05 12:27:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        nhai: {
+          50: '#eef4ff',
+          100: '#dfe8ff',
+          200: '#c4d4ff',
+          300: '#97b3ff',
+          400: '#688dff',
+          500: '#3b66ff',
+          600: '#304ce6',
+          700: '#283db0',
+          800: '#223389',
+          900: '#1f2f71'
+        }
+      }
+    }
+  },
+  plugins: []
+};
